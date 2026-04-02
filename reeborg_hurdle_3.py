6afc43e9ca605a6fb10ffd1cf22bce133f1a14b0
saicharanuchiha <saicherrin007@gmail.com>
2026-04-02 13:15:39 +0530

    Solved Reeborg Hurdle 3 using while loops and conditional logic

---
+++ reeborg_hurdle_3.py
@@ -0,0 +1,23 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+while not at_goal():
+    if front_is_clear():
+        move()
+        if wall_in_front():
+            jump()
+    else:
+        jump()
